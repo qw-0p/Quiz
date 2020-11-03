564cd567bbc7f9c1a278524b12a62327513ca1d8
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-03 19:03:24 +0200

    created FinishedQuiz

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './FinishedQuiz.module.css'
+
+const FinishedQuiz = () => {
+    return <div className={classes.FinishedQuiz}></div>
+}
+
+export default FinishedQuiz
