592c46eee651595e502cfb7f608b39539f800f57
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-06 18:41:43 +0200

    set up links

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react'
 import Backdrop from '../../UI/Backdrop/Backdrop'
+import {NavLink} from 'react-router-dom'
 import classes from './Drawer.module.css'
 
-const links = [1, 2, 3]
+const links = [
+    {to: '/', label: 'Список', exact: true},
+    {to: '/auth', label: 'Авторизация', exact: false},
+    {to: '/quiz-creator', label: 'Создать тест', exact: false}
+]
 
 class Drawer extends Component {
+    clickHandler = () => {
+        this.props.onClose()
+    }
     renderLinks() {
         return links.map((link, index) => {
-            return <li key={index}>Link {link}</li>
+            return (
+                <li key={index}>
+                    <NavLink to={link.to} exact={link.exact} activeClassName={classes.active} onClick={this.clickHandler}>
+                        {link.label}
+                    </NavLink>
+                </li>
+            )
         })
     }
     render() {
