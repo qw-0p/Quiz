0eac5289dfc13bd2645597613d0c440b9bdf09fa
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-07 13:32:25 +0200

    set up registration page, and styled it

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,32 @@
 import React, {Component} from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class Auth extends Component {
+    loginHandler = () => {}
+    registerHandler = () => {}
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
     render() {
-        return <div></div>
+        return (
+            <div className={classes.Auth}>
+                <h1>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type='text' />
+                        <input type='text' />
+                        <Button type='success' onClick={this.loginHandler}>
+                            Войти
+                        </Button>
+                        <Button type='primary' onClick={this.registerHandler}>
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </h1>
+            </div>
+        )
     }
 }
 export default Auth
