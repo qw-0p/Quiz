acf8b32361c342ebac87de79dba810d3ea359a75
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-07 16:31:43 +0200

    added validation for buttons

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
 class Auth extends Component {
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -67,8 +68,15 @@ class Auth extends Component {
 
         formControls[controlName] = control
 
+        let isFormValid = true
+
+        Object.keys(formControls).forEach((name) => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls,
+            isFormValid
         })
     }
 
@@ -99,10 +107,10 @@ class Auth extends Component {
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
                         {this.renderInputs()}
 
-                        <Button type='success' onClick={this.loginHandler}>
+                        <Button type='success' onClick={this.loginHandler} disabled={!this.state.isFormValid}>
                             Войти
                         </Button>
-                        <Button type='primary' onClick={this.registerHandler}>
+                        <Button type='primary' onClick={this.registerHandler} disabled={!this.state.isFormValid}>
                             Зарегистрироваться
                         </Button>
                     </form>
