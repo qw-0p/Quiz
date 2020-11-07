6029b2a25a96fa2bfaa622f3ad477d5d14662842
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-07 17:55:53 +0200

    set up select input

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -4,6 +4,7 @@ import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import {createControl} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import Select from '../../components/UI/Select/Select'
 
 function createOptionControl(number) {
     return createControl(
@@ -35,6 +36,7 @@ function createFormControls() {
 export default class QuizCreator extends Component {
     state = {
         quiz: [],
+        rightAnswerId: 1,
         formControls: createFormControls()
     }
 
@@ -68,8 +70,26 @@ export default class QuizCreator extends Component {
             )
         })
     }
+    selectChangeHanlder = (event) => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
 
     render() {
+        const select = (
+            <Select
+                labe='Выберите правильный ответ'
+                value={this.state.rightAnswerId}
+                onChange={this.selectChangeHanlder}
+                options={[
+                    {text: 1, value: 1},
+                    {text: 2, value: 2},
+                    {text: 3, value: 3},
+                    {text: 4, value: 4}
+                ]}
+            />
+        )
         return (
             <div className={classes.QuizCreator}>
                 <div>
@@ -77,8 +97,7 @@ export default class QuizCreator extends Component {
 
                     <form onSubmit={this.submitHandler}>
                         {this.renderControls()}
-
-                        <select></select>
+                        {select}
 
                         <Button type='primary' onClick={this.addQuestionHandler}>
                             Добавить вопрос
