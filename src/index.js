73cd938f61a5c5ede5fbddad9b96232b1e30298b
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-11 20:55:56 +0200

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import {BrowserRouter} from 'react-router-dom'
 import {createStore, compose, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
 
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
     <Provider store={store}>
