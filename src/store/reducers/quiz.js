bd394d54ebbc37a3e16a746923c0d82aba983868
Aleksandr Sarkisov <quopiks@gmail.com>
2020-11-12 17:20:18 +0200

    refactoring component

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,4 +1,13 @@
-import {FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS, FETCH_QUIZ_SUCCESS} from '../actions/actionTypes'
+import {
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS,
+    FINISH_QUIZ,
+    QUIZ_SET_STATE,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY
+} from '../actions/actionTypes'
 
 const initialState = {
     quizes: [],
@@ -35,6 +44,31 @@ export default function quizReducer(state = initialState, action) {
                 ...state,
                 loading: false,
                 quiz: action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state,
+                answerState: action.answerState,
+                results: action.results
+            }
+        case FINISH_QUIZ:
+            return {
+                ...state,
+                isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state,
+                answerState: null,
+                activeQuestion: action.number
+            }
+        case QUIZ_RETRY:
+            return {
+                ...state,
+                results: {},
+                isFinished: false,
+                activeQuestion: 0,
+                answerState: null
             }
         default:
             return state
